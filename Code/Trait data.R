2c3bc7427b0f2af883a193d8589a4cc11ad686f4
Joshua-AE <j-erkelenz@gmx.de>
2023-05-11 17:51:28 +0200

    First commit to upload existing files to repository

---
+++ Code/Trait data.R
@@ -0,0 +1,8 @@
+library("tidyverse")
+trait <- read_tsv("C:/Users/user/Documents/PhD/Travels/Bilbao/Trait data.txt", guess_max = Inf)
+trait$sp_name <- paste(trait$Genus, trait$Species, sep = " ") # combined species name
+LDMC <- trait %>% group_by(sp_name) %>% summarise(LDMC_mean = mean(LDMC), LDMC_std = sqrt(var(LDMC)))
+trait <- left_join(trait, LDMC, by = "sp_name")
+trait$out <- case_when(trait$LDMC > (trait$LDMC_mean + trait$LDMC_std*2) ~ "up",
+                       trait$LDMC < (trait$LDMC_mean - trait$LDMC_std*2) ~ "down") 
+# determines outliers that are below (down) or above (up) mean LDMC +- 2* standard deviaton
